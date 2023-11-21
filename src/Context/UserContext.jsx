7219d1bfbe6607887c65b51efeaa95d9f2eef1db
Rafael Sales <2017infor45@gmail.com>
2023-11-21 18:08:40 -0300

    Projeto iniciado com as configurações estabelecidas

---
+++ src/Context/UserContext.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const UserContext = React.createContext();
+
+export const UserStorage = ({ children }) => {
+  return <UserContext.Provider>{children}</UserContext.Provider>;
+};
